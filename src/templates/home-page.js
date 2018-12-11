354c39e817c1988c1f0d5c6757a1b7240a69463f
4levels <4levels@gmail.com>
2018-12-11 12:00:26 +0100

    [WIP] reafactor homepage with carousel & info block

--- src/templates/home-page.js
+++ src/templates/home-page.js
@@ -2,52 +2,88 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
+import Features from '../components/Features'
+import Testimonials from '../components/Testimonials'
 
-export const HomePageTemplate = ({ title, content, contentComponent }) => {
-  const PageContent = contentComponent || Content
-  console.log(PageContent)
-
-  return (
-    <section className="section section--gradient">
-      <div className="container">
+export const HomePageTemplate = ({
+  title,
+  carousel,
+  info,
+  body,
+  testimonials,
+}) => (
+  <section className="section section--gradient">
+    <div className="container">
+      <div className="section">
         <div className="columns">
           <div className="column is-10 is-offset-1 main-content">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
+            <div className="content">
+              <Features gridItems={carousel.images} />
+              <h2>
+                  {title}
               </h2>
-              <PageContent className="content" content={content} />
+              <div className="columns">
+                <div className="column is-7">
+                  <h3 className="has-text-weight-semibold is-size-2">
+                    {info.heading}
+                  </h3>
+                  <p>{info.description}</p>
+                </div>
+              </div>
+              <Features gridItems={info.blurbs} />
+              <div className="columns">
+                <div className="column is-7">
+                  <h3 className="has-text-weight-semibold is-size-3">
+                    {info.heading}
+                  </h3>
+                  <p>{info.description}</p>
+                </div>
+              </div>
+              <Testimonials testimonials={testimonials} />
             </div>
           </div>
         </div>
       </div>
-    </section>
-  )
-}
+    </div>
+  </section>
+)
 
 HomePageTemplate.propTypes = {
-  title: PropTypes.string.isRequired,
-  content: PropTypes.string,
-  contentComponent: PropTypes.func,
+  title: PropTypes.string,
+  body: PropTypes.string,
+  carousel: PropTypes.shape({
+    images: PropTypes.array,
+  }),
+  info: PropTypes.shape({
+    heading: PropTypes.string,
+    description: PropTypes.string,
+    blurbs: PropTypes.array,
+  }),
+  testimonials: PropTypes.array,
 }
 
 const HomePage = ({ data }) => {
-  const { markdownRemark: post } = data
+  const { frontmatter } = data.markdownRemark
 
   return (
     <Layout>
       <HomePageTemplate
-        contentComponent={HTMLContent}
-        title={post.frontmatter.title}
-        content={post.html}
+        title={frontmatter.title}
+        body={frontmatter.body}
+        info={frontmatter.info}
+        carousel={frontmatter.carousel}
+        testimonials={frontmatter.testimonials}
       />
     </Layout>
   )
 }
 
 HomePage.propTypes = {
-  data: PropTypes.object.isRequired,
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
+    }),
+  }),
 }
 
 export default HomePage
@@ -55,9 +91,39 @@ export default HomePage
 export const homePageQuery = graphql`
   query HomePage($id: String!) {
     markdownRemark(id: { eq: $id }) {
-      html
       frontmatter {
         title
+        body
+        carousel {
+          images {
+            image {
+              childImageSharp {
+                fluid(maxWidth: 240, quality: 64) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            text
+          }
+        }
+        intro {
+          blurbs {
+            image {
+              childImageSharp {
+                fluid(maxWidth: 240, quality: 64) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            text
+          }
+          heading
+          description
+        }
+        testimonials {
+          author
+          quote
+        }
       }
     }
   }
