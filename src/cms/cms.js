fca0e89ca691b6b1eb340f746bc4ee5a0b6328f7
4levels <4levels@gmail.com>
2018-12-11 11:31:08 +0100

    Added homepage preview component

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,10 +1,12 @@
 import CMS from 'netlify-cms'
 
+import HomePagePreview from './preview-templates/HomePagePreview'
 import ClassesPagePreview from './preview-templates/ClassesPagePreview'
 import SchoolPagePreview from './preview-templates/SchoolPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import PedagogyPagePreview from './preview-templates/PedagogyPagePreview'
 
+CMS.registerPreviewTemplate('home', HomePagePreview)
 CMS.registerPreviewTemplate('classes', ClassesPagePreview)
 CMS.registerPreviewTemplate('school', SchoolPagePreview)
 CMS.registerPreviewTemplate('pedagogy', PedagogyPagePreview)
