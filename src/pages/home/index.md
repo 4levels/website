5e5e80592acdb6a6063060f47ebecf051aa38e46
4levels <4levels@gmail.com>
2018-12-11 12:32:21 +0100

    [WIP] Homepage extensions

--- src/pages/home/index.md
+++ src/pages/home/index.md
@@ -4,22 +4,25 @@ title: Welkom op onze school
 carousel:
   images:
     - image: /img/chemex.jpg
+      text: ''
     - image: /img/flavor_wheel.jpg
+      text: ''
     - image: /img/jumbotron.jpg
+      text: ''
 info:
   description: ''
   heading: Bedankt voor je bezoek op onze website!
 ---
 Onze school  werkt vanuit de ideëen van Rudolf Steiner.\
 Zijn pedagogische visie was dat elk kind zich moet kunnen ontwikkelen tot een innerlijk vrij, zelfstandig en verantwoordelijk mens.
-Cognitieve vaardigheden zijn hierbij gelijkwaardig aan emotionele, sociale en kunstzinnige vorming. 
-Wij proberen kinderen een warme en zo veelzijdig mogelijke leeromgeving te bieden, waarin elk kind zijn eigen talenten kan ontwikkelen. 
+Cognitieve vaardigheden zijn hierbij gelijkwaardig aan emotionele, sociale en kunstzinnige vorming.
+Wij proberen kinderen een warme en zo veelzijdig mogelijke leeromgeving te bieden, waarin elk kind zijn eigen talenten kan ontwikkelen.
 
 ### Schoolbestuur
 
-De Teunisbloem wordt beheerd door leerkrachten, medewerkers en ouders samen. 
-Er is geen hiërarchie, geen 'centraal gezag', wel een advies- en overlegcultuur waarin de schoolleiding/directie de bevoegdheid 
-heeft beslissingen en beleidslijnen uit te voeren die ontstaan zijn binnen de lerarenvergadering, het schoolbestuur of 
-de ouderwerking. Centraal staan 'ware' communicatie en ontmoeting. 
-Dit is alleen mogelijk vanuit waardering voor ieders eigenheid en integriteit. 
+De Teunisbloem wordt beheerd door leerkrachten, medewerkers en ouders samen.
+Er is geen hiërarchie, geen 'centraal gezag', wel een advies- en overlegcultuur waarin de schoolleiding/directie de bevoegdheid
+heeft beslissingen en beleidslijnen uit te voeren die ontstaan zijn binnen de lerarenvergadering, het schoolbestuur of
+de ouderwerking. Centraal staan 'ware' communicatie en ontmoeting.
+Dit is alleen mogelijk vanuit waardering voor ieders eigenheid en integriteit.
 Meer nog, ze vormen de kwaliteit van het schoolleven
--- src/templates/home-page.js
+++ src/templates/home-page.js
@@ -2,57 +2,70 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
+import Content, { HTMLContent } from '../components/Content'
 import Features from '../components/Features'
 import Testimonials from '../components/Testimonials'
 
 export const HomePageTemplate = ({
   title,
+  content,
+  contentComponent,
   carousel,
   info,
   body,
   testimonials,
-}) => (
-  <section className="section section--gradient">
-    <div className="container">
-      <div className="section">
-        <div className="columns">
-          <div className="column is-10 is-offset-1 main-content">
-            <div className="content">
-              <Features gridItems={carousel.images} />
-              <h2>
-                  {title}
-              </h2>
-              <div className="columns">
-                <div className="column is-7">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    {info.heading}
-                  </h3>
-                  <p>{info.description}</p>
+}) => {
+
+  const PageContent = contentComponent || Content
+
+  return (
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="section">
+          <div className="columns">
+            <div className="column is-10 is-offset-1 main-content">
+              <div className="content">
+                <Features gridItems={carousel.images} />
+                <h2>
+                    {title}
+                </h2>
+                <div className="columns">
+                  <div className="column is-7">
+                    <h3 className="has-text-weight-semibold is-size-2">
+                      {info.heading}
+                    </h3>
+                    <p>{info.description}</p>
+                  </div>
                 </div>
-              </div>
-              <Features gridItems={info.blurbs} />
-              <div className="columns">
-                <div className="column is-7">
-                  <h3 className="has-text-weight-semibold is-size-3">
-                    {info.heading}
-                  </h3>
-                  <p>{info.description}</p>
+                <PageContent className="content" content={content} />
+                <Features gridItems={info.blurbs} />
+                <div className="columns">
+                  <div className="column is-7">
+                    <h3 className="has-text-weight-semibold is-size-3">
+                      {info.heading}
+                    </h3>
+                    <p>{info.description}</p>
+                  </div>
                 </div>
+                <Testimonials testimonials={testimonials} />
               </div>
-              <Testimonials testimonials={testimonials} />
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
-)
+    </section>
+  )
+}
 
 HomePageTemplate.propTypes = {
-  title: PropTypes.string,
-  body: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string,
+  contentComponent: PropTypes.func,
   carousel: PropTypes.shape({
-    images: PropTypes.array,
+    images: PropTypes.arrayOf(PropTypes.shape({
+      image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+      text: PropTypes.string,
+    })),
   }),
   info: PropTypes.shape({
     heading: PropTypes.string,
@@ -64,12 +77,14 @@ HomePageTemplate.propTypes = {
 
 const HomePage = ({ data }) => {
   const { frontmatter } = data.markdownRemark
+  const { markdownRemark: post } = data
 
   return (
     <Layout>
       <HomePageTemplate
-        title={frontmatter.title}
-        body={frontmatter.body}
+        title={post.frontmatter.title}
+        content={post.html}
+        contentComponent={HTMLContent}
         info={frontmatter.info}
         carousel={frontmatter.carousel}
         testimonials={frontmatter.testimonials}
@@ -91,9 +106,9 @@ export default HomePage
 export const homePageQuery = graphql`
   query HomePage($id: String!) {
     markdownRemark(id: { eq: $id }) {
+      html
       frontmatter {
         title
-        body
         carousel {
           images {
             image {
