a7255c4989d9925836e4886e3f54be0419331fde
4levels <4levels@gmail.com>
2018-12-22 11:38:54 +0100

    Added Slides custom widget Added content type Renamed Blog to Berichten

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,11 +1,13 @@
 import CMS from 'netlify-cms'
 
+import { SlidesControl, SlidesPreview } from './Slides'
 import HomePagePreview from './preview-templates/HomePagePreview'
 import ClassesPagePreview from './preview-templates/ClassesPagePreview'
 import SchoolPagePreview from './preview-templates/SchoolPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import PedagogyPagePreview from './preview-templates/PedagogyPagePreview'
 
+CMS.registerWidget("slides", SlidesControl, SlidesPreview);
 CMS.registerPreviewTemplate('home', HomePagePreview)
 CMS.registerPreviewTemplate('classes', ClassesPagePreview)
 CMS.registerPreviewTemplate('school', SchoolPagePreview)
